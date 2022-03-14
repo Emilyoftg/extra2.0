c61fe88c0892f80f86d4577048ba484c3f4f3b63
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-03-14 12:31:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Emilyoftg/extra.git /extra
+  git clone https://github.com/Emilyoftg/extra2.0.git /extra2.0
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /extra
+  git clone $UPSTREAM_REPO /extra2.0
 fi
-cd /Ajax
+cd /extra2.0
 pip3 install -U -r requirements.txt
 echo "Starting Master 🤟"
 python3 bot.py
